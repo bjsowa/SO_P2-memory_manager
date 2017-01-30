3a8b3ec59d40fa1dbc1dc9867f25d843a6bd6d04
vintageblues <vintageblues@users.noreply.github.com>
2017-01-30 10:19:56 +0100

    Kolorowanie
    Kolorowanie jeszcze czasem nie działa

--- dynmem/stats.c
+++ dynmem/stats.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "stats.h"
 #include "structs.h"
 
+#define RED_TEXT   "\x1B[31m"
+#define RED_BLINK   "\x1B[5;31m"
+#define GRN_TEXT   "\x1B[32m"
+#define GRN_BLINK   "\x1B[5;32m"
+#define RED   "\x1B[7;31m"
+#define GRN   "\x1B[7;32m"
+#define BLU   "\x1B[7;34m"
+#define RESET "\x1B[0m"
+
 int areasCreated = 0;
+int blocksDivided = 0;
+int blocksMerged = 0;
 
 void showAddress(void* ptr)
 {
@@ -17,6 +29,8 @@ void showAddress(void* ptr)
 void printStats()
 {
 	printf("Areas Created: %d\n", areasCreated);
+	printf("Blocks Merged: %d\n", blocksMerged);
+	printf("Blocks Divided: %d\n", blocksDivided);       
 }
 
 void printBlocks()
@@ -25,22 +39,65 @@ void printBlocks()
 	area* currentArea = firstArea;
 	while( currentArea != NULL )
 	{
-		printf("AREA %d\n\n", i);
-		int j = 1;
+        printf(BLU);
+        int h=53;
+		while(h--)
+            printf(" ");
+        printf("AREA %d", i);
+        h=53;
+		while(h--)
+            printf(" ");
+        printf(RESET "\n");
+		int j = 1, spaceForText=100;
+        char str[12];
 		block* currentBlock = currentArea->firstBlock;
+
+        int allBlocks = 0;
+
+        while( currentBlock != NULL ){
+                currentBlock = currentBlock->next;
+                allBlocks++;
+        }
+
+        currentBlock = currentArea->firstBlock;
+
 		while( currentBlock != NULL )
 		{
-			printf("BLOCK %d\nTYPE: ", j);
-			if( currentBlock->size < 0 )
-				printf("FREE\n");
-			else 
-				printf("TAKEN\n");
-
-			printf("SIZE: %d\n\n", abs(currentBlock->size));
+            printf(RESET);
+			if( currentBlock->size < 0 ){
+				printf(GRN);
+            }
+			else{
+				printf(RED);
+            }
+            snprintf(str, 12, " SIZE: %d", abs(currentBlock->size));
+            printf(str);
+            h=((100*abs(currentBlock->size))/4016)-allBlocks*strlen(str)-1;
+            if(spaceForText<h)
+                h=spaceForText;
+            if(h<0)
+                spaceForText-=strlen(str)+1;
+            else {
+                while(spaceForText>0 && h>0){
+                printf(" ");
+                --h;
+                --spaceForText;
+            }}
+            allBlocks--;
 			currentBlock = currentBlock->next;
 			j++;
+            if( currentBlock != NULL ){
+                printf("|");
+            }
 		}
+        while(spaceForText>0){
+            printf(" ");
+            spaceForText--;
+        }
+        printf("|");
+        printf(RESET);
 		currentArea = currentArea->next;
 		i++;
+        printf("\n");
 	}
 }
