e4473a2c9ab999a3c97091c91ccc349847f3f31c
vintageblues <vintageblues@users.noreply.github.com>
2017-01-31 13:10:08 +0100

    Update stats.h

--- dynmem/stats.h
+++ dynmem/stats.h
@@ -8,9 +8,20 @@ void printStats();
 void printBlocks();
 
 extern int areasCreated;
+extern int areasUnmapped;
+
 extern uint64_t takenSpace;
 extern uint64_t freeSpace;
 extern int blocksDivided;
 extern int blocksMerged;
+
+extern uint64_t maxTakenSpace;
+extern uint64_t maxFreeSpace;
+
+extern int mallocCalls;
+extern int callocCalls;
+extern int reallocCalls;
+extern int posix_memalignCalls;
+extern int freeCalls;
 
 #endif
